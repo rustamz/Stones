3619d0d58f508462aba7af8f5c74e75a27eec01f
Рустам З. <rust.89@live.ru>
2013-03-31 13:13:36 +0600

    Первая заливка

---
+++ Stones/MainForm.Designer.cs
@@ -0,0 +1,186 @@
+﻿namespace Stones
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pnlImageContainer = new System.Windows.Forms.Panel();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.tsmiFile = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsmiOpen = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripSeparator();
+            this.tsmiClose = new System.Windows.Forms.ToolStripMenuItem();
+            this.pbMainImage = new System.Windows.Forms.PictureBox();
+            this.sbStatusBar = new System.Windows.Forms.StatusStrip();
+            this.tsslFileName = new System.Windows.Forms.ToolStripStatusLabel();
+            this.tsmiService = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsmiMakeGray = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsmiMonochrome = new System.Windows.Forms.ToolStripMenuItem();
+            this.pnlImageContainer.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbMainImage)).BeginInit();
+            this.sbStatusBar.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pnlImageContainer
+            // 
+            this.pnlImageContainer.AutoScroll = true;
+            this.pnlImageContainer.Controls.Add(this.pbMainImage);
+            this.pnlImageContainer.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pnlImageContainer.Location = new System.Drawing.Point(0, 24);
+            this.pnlImageContainer.Name = "pnlImageContainer";
+            this.pnlImageContainer.Size = new System.Drawing.Size(494, 337);
+            this.pnlImageContainer.TabIndex = 0;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tsmiFile,
+            this.tsmiService});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(494, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // tsmiFile
+            // 
+            this.tsmiFile.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tsmiOpen,
+            this.toolStripMenuItem1,
+            this.tsmiClose});
+            this.tsmiFile.Name = "tsmiFile";
+            this.tsmiFile.Size = new System.Drawing.Size(48, 20);
+            this.tsmiFile.Text = "Файл";
+            // 
+            // tsmiOpen
+            // 
+            this.tsmiOpen.Name = "tsmiOpen";
+            this.tsmiOpen.Size = new System.Drawing.Size(152, 22);
+            this.tsmiOpen.Text = "Открыть";
+            this.tsmiOpen.Click += new System.EventHandler(this.tsmiOpen_Click);
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(149, 6);
+            // 
+            // tsmiClose
+            // 
+            this.tsmiClose.Name = "tsmiClose";
+            this.tsmiClose.Size = new System.Drawing.Size(152, 22);
+            this.tsmiClose.Text = "Закрыть";
+            this.tsmiClose.Click += new System.EventHandler(this.tsmiClose_Click);
+            // 
+            // pbMainImage
+            // 
+            this.pbMainImage.Location = new System.Drawing.Point(0, 0);
+            this.pbMainImage.Name = "pbMainImage";
+            this.pbMainImage.Size = new System.Drawing.Size(414, 264);
+            this.pbMainImage.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pbMainImage.TabIndex = 0;
+            this.pbMainImage.TabStop = false;
+            // 
+            // sbStatusBar
+            // 
+            this.sbStatusBar.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tsslFileName});
+            this.sbStatusBar.Location = new System.Drawing.Point(0, 339);
+            this.sbStatusBar.Name = "sbStatusBar";
+            this.sbStatusBar.Size = new System.Drawing.Size(494, 22);
+            this.sbStatusBar.TabIndex = 2;
+            this.sbStatusBar.Text = "statusStrip1";
+            // 
+            // tsslFileName
+            // 
+            this.tsslFileName.Name = "tsslFileName";
+            this.tsslFileName.Size = new System.Drawing.Size(69, 17);
+            this.tsslFileName.Text = "Имя файла";
+            // 
+            // tsmiService
+            // 
+            this.tsmiService.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tsmiMakeGray,
+            this.tsmiMonochrome});
+            this.tsmiService.Name = "tsmiService";
+            this.tsmiService.Size = new System.Drawing.Size(59, 20);
+            this.tsmiService.Text = "Сервис";
+            // 
+            // tsmiMakeGray
+            // 
+            this.tsmiMakeGray.Name = "tsmiMakeGray";
+            this.tsmiMakeGray.Size = new System.Drawing.Size(152, 22);
+            this.tsmiMakeGray.Text = "Обесцветить";
+            this.tsmiMakeGray.Click += new System.EventHandler(this.tsmiMakeGray_Click);
+            // 
+            // tsmiMonochrome
+            // 
+            this.tsmiMonochrome.Name = "tsmiMonochrome";
+            this.tsmiMonochrome.Size = new System.Drawing.Size(152, 22);
+            this.tsmiMonochrome.Text = "В монохром";
+            this.tsmiMonochrome.Click += new System.EventHandler(this.tsmiMonochrome_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(494, 361);
+            this.Controls.Add(this.sbStatusBar);
+            this.Controls.Add(this.pnlImageContainer);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "MainForm";
+            this.Text = "Stones";
+            this.pnlImageContainer.ResumeLayout(false);
+            this.pnlImageContainer.PerformLayout();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbMainImage)).EndInit();
+            this.sbStatusBar.ResumeLayout(false);
+            this.sbStatusBar.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pnlImageContainer;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem tsmiFile;
+        private System.Windows.Forms.ToolStripMenuItem tsmiOpen;
+        private System.Windows.Forms.ToolStripSeparator toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem tsmiClose;
+        private System.Windows.Forms.PictureBox pbMainImage;
+        private System.Windows.Forms.StatusStrip sbStatusBar;
+        private System.Windows.Forms.ToolStripStatusLabel tsslFileName;
+        private System.Windows.Forms.ToolStripMenuItem tsmiService;
+        private System.Windows.Forms.ToolStripMenuItem tsmiMakeGray;
+        private System.Windows.Forms.ToolStripMenuItem tsmiMonochrome;
+    }
+}
+
---
+++ Stones/MainForm.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Runtime.InteropServices;
+
+
+namespace Stones
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void tsmiClose_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void tsmiOpen_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "Файлы изображений (*.bmp, *.jpg, *.png)|*.bmp;*.jpg;*.png";
+            if (ofd.ShowDialog(this) == System.Windows.Forms.DialogResult.OK)
+            {
+                tsslFileName.Text = System.IO.Path.GetFileName(ofd.FileName);
+                pbMainImage.Load(ofd.FileName);
+            }
+        }
+
+        private void tsmiMakeGray_Click(object sender, EventArgs e)
+        {
+            Bitmap image = new Bitmap(pbMainImage.Image);
+            if (image != null)
+            {
+                MakeGray(image);
+                pbMainImage.Image = image;
+            }
+        }
+
+        private void MakeGray(Bitmap bmp)
+        {
+            // Задаём формат Пикселя.
+            PixelFormat pxf = PixelFormat.Format24bppRgb;
+
+            // Получаем данные картинки.
+            Rectangle rect = new Rectangle(0, 0, bmp.Width, bmp.Height);
+            //Блокируем набор данных изображения в памяти
+            BitmapData bmpData = bmp.LockBits(rect, ImageLockMode.ReadWrite, pxf);
+
+            // Получаем адрес первой линии.
+            IntPtr ptr = bmpData.Scan0;
+
+            // Задаём массив из Byte и помещаем в него надор данных.
+            // int numBytes = bmp.Width * bmp.Height * 3; 
+            //На 3 умножаем - поскольку RGB цвет кодируется 3-мя байтами
+            //Либо используем вместо Width - Stride
+            int numBytes = bmpData.Stride * bmp.Height;
+            int widthBytes = bmpData.Stride;
+            byte[] rgbValues = new byte[numBytes];
+
+            // Копируем значения в массив.
+            Marshal.Copy(ptr, rgbValues, 0, numBytes);
+
+            // Перебираем пикселы по 3 байта на каждый и меняем значения
+            for (int counter = 0; counter < rgbValues.Length; counter += 3)
+            {
+
+                int value = rgbValues[counter] + rgbValues[counter + 1] + rgbValues[counter + 2];
+                byte color_b = 0;
+
+                color_b = Convert.ToByte(value / 3);
+
+                rgbValues[counter] = color_b;
+                rgbValues[counter + 1] = color_b;
+                rgbValues[counter + 2] = color_b;
+
+            }
+            // Копируем набор данных обратно в изображение
+            Marshal.Copy(rgbValues, 0, ptr, numBytes);
+
+            // Разблокируем набор данных изображения в памяти.
+            bmp.UnlockBits(bmpData);
+        }
+
+        private void MakeMonochrome(Bitmap bmp, byte Bound)
+        {
+            // Задаём формат Пикселя.
+            PixelFormat pxf = PixelFormat.Format24bppRgb;
+
+            // Получаем данные картинки.
+            Rectangle rect = new Rectangle(0, 0, bmp.Width, bmp.Height);
+            //Блокируем набор данных изображения в памяти
+            BitmapData bmpData = bmp.LockBits(rect, ImageLockMode.ReadWrite, pxf);
+
+            // Получаем адрес первой линии.
+            IntPtr ptr = bmpData.Scan0;
+
+            // Задаём массив из Byte и помещаем в него надор данных.
+            // int numBytes = bmp.Width * bmp.Height * 3; 
+            // На 3 умножаем - поскольку RGB цвет кодируется 3-мя байтами
+            // Либо используем вместо Width - Stride
+            int numBytes = bmpData.Stride * bmp.Height;
+            int widthBytes = bmpData.Stride;
+            byte[] rgbValues = new byte[numBytes];
+
+            // Копируем значения в массив.
+            Marshal.Copy(ptr, rgbValues, 0, numBytes);
+
+            // Перебираем пикселы по 3 байта на каждый и меняем значения
+            for (int counter = 0; counter < rgbValues.Length; counter += 3)
+            {
+
+                int value = rgbValues[counter] + rgbValues[counter + 1] + rgbValues[counter + 2];
+                byte color_b = 0;
+
+                color_b = Convert.ToByte(value / 3);
+
+                if (color_b > Bound)
+                    color_b = 255;
+                else
+                    color_b = 0;
+
+                rgbValues[counter] = color_b;
+                rgbValues[counter + 1] = color_b;
+                rgbValues[counter + 2] = color_b;
+
+            }
+            // Копируем набор данных обратно в изображение
+            Marshal.Copy(rgbValues, 0, ptr, numBytes);
+
+            // Разблокируем набор данных изображения в памяти.
+            bmp.UnlockBits(bmpData);
+        }
+
+        private void tsmiMonochrome_Click(object sender, EventArgs e)
+        {
+            Bitmap image = new Bitmap(pbMainImage.Image);
+            if (image != null)
+            {
+                MakeMonochrome(image, 127);
+                pbMainImage.Image = image;
+            }
+        }
+    }
+}
